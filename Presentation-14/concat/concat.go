c467df5eb12ca47bdac7b86a396caf80b7375427
Vinit13 <vinitnair_13@mail.fresnostate.edu>
2015-12-10 02:19:46 -0800

    Presentation 14

---
+++ Presentation-14/concat/concat.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+
+func main(){
+	first := "Viinit"
+	last := "Nair"
+	nickName := "Vinya"
+
+	fmt.Println("My name is " + first + " " + last)
+	fmt.Println("My English name is " + nickName + " " + last)
+}
---
+++ Presentation-14/len/len.go
@@ -0,0 +1,8 @@
+package main
+import "fmt"
+
+func main(){
+	firstName:= "Vinit"
+	lastName:= "Nair"
+	fmt.Println("The length of my name is ", len(firstName) + len(lastName))
+}
